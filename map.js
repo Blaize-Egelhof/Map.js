67775b336d8f1043667fe2a60d5952f05e0bbc9d
Blaize Egelhof <103180767+Blaize-Egelhof@users.noreply.github.com>
2023-09-11 09:55:31 +0000

    finished notes for map.js

--- map.js
+++ map.js
@@ -6,15 +6,39 @@
 
 // Using a for loop
 
+let nums = [1,2,3,4];
+let results= [];
+
+for(let x of nums) {
+  results.push(x*2); 
+}
+
+console.log(results);
 
 // Using map()
+
+const multByTwo = function (num) {
+return num * 2 ;
+}
+
+const mapResults = nums.map(multByTwo);
+console.log(mapResults)
+
+
 
 
 // Simplified w/ map()
 
 
+const simplified = nums.map(function (num){return num * 2});  //basically just create a function to do something on each iteration , seems that this is a fixed for of loop when using map.js so keep that in mind when creating these functions.
+
+console.log(simplified)
+
 // Simplfied w/ map() + arrow function
 
+const simplified2 = nums.map(num => num * 2 ); 
+
+console.log(simplified2)
 
 // With objects:
 const students = [
@@ -37,3 +61,7 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name + student.id]);
+
+console.log(studentsWithIds)
